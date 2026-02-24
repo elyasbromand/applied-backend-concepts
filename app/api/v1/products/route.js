4dbe1900c642506d88cf2becfe22e1a4a0fb72a4
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2026-02-24 11:11:51 +0430

    Remove manual old authentication

--- app/api/v1/products/route.js
+++ app/api/v1/products/route.js
@@ -2,9 +2,6 @@ import { getProductsForVersion } from "../../../../lib/products.js";
 
 export async function GET(request) {
   try {
-    // for protecting the route
-    // const auth = await requireAuth(request);
-    // if (auth instanceof Response) return auth;
 
     const data = getProductsForVersion("v1");
     const body = {
